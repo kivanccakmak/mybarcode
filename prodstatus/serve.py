5b1f83b217dbae615637f6ab9471a8f6bcd1e650
Kivanc Cakmak <kivanccakmak@gmail.com>
2018-11-21 23:01:39 +0300

    initial commit on productstatus

---
+++ prodstatus/serve.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+"""
+server
+"""
+import socket, fcntl, struct
+import logging
+import json
+import os
+import sys
+import pymssql
+from flask import Flask, jsonify, render_template,\
+    request, send_from_directory, send_file
+import ConfigParser
+import ast
+
+APP = Flask(__name__)
+
+# LOGGING CONFIGURATION
+FORMAT = '%(asctime)-30s %(levelname)-10s ' +\
+        '[%(filename)-20s %(funcName)25s()] ' +\
+        '%(message)s'
+logging.basicConfig(filename="serve.log", level=logging.INFO, format=FORMAT)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+"""-------------------------------------------------"""
+@APP.route('/upload', methods=['POST'])
+def supply_data():
+    """
+    supply packet information with airtimes.
+    :return: json
+    """
+    return jsonify(result="abc")
+
+@APP.route("/record.html")
+def index():
+    with open("data/record.json", "r") as data_file:
+        data = json.load(data_file)
+    return render_template('record.html', 
+            colnames=data['colnames'], 
+            records=data['records'])
+
+#   -------------------------------------------------
+
+
+
+def get_interface_ip(ifname):
+    """ip address of interface
+    :ifname: str
+    :return: str
+    This function gets IP addr of interface
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(sock.fileno(),\
+            0x8915, struct.pack('256s', ifname[:15]))[20:24])
+
+def get_config(cfile, sections):
+    """read config.ini which contains required meta-data
+    to serve UI, relative paths of raw/processed data,
+    server configuration.
+    :cfile: Str[]
+    :sections: Str[] or Str
+    :return: dict
+        configuration
+    """
+    pack_info, config = {}, {}
+    try:
+        parser = ConfigParser.SafeConfigParser()
+        parser.read(cfile)
+    except ConfigParser.ParsingError, err:
+        print('Could not parse:', err)
+        return None
+
+    if type(sections) is list:
+        for elem in sections:
+            for name, value in parser.items(elem):
+                config[name] = value
+    elif type(sections) is str:
+        for name, value in parser.items(sections):
+            config[name] = value
+
+    return config
+
+def main():
+    """main func"""
+    config = get_config("config.ini", ["server"])
+
+    APP.run(
+            host=get_interface_ip(config['interface']),
+            port=int(config['port']),
+            debug=True
+            )
+
+if __name__ == "__main__":
+    main()
